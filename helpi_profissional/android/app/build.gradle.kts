ae80119e6fb819c200cb62080b5c489f11689585
Paulinhenry <paulo2403santos@gmail.com>
2026-06-19 21:21:06 -0300

    v 34 do SDK trade

--- helpi_profissional/android/app/build.gradle.kts
+++ helpi_profissional/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.helpi_profissional"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 34
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
